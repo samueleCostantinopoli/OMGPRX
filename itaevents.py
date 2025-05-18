712bb97fb412fc1e030ee112e7cd7f87cdfd4bd9
samueleCostantinopoli <116150522+samueleCostantinopoli@users.noreply.github.com>
2025-05-18 16:07:36 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 # Constants
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://victoria2552-sallo.hf.space"
+PASSWORD = "mario"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 REFERER = "forcedtoplay.xyz"
